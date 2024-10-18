a5caaadb7385ccc78c19a67a1f77f23fa868d564
Talaku Getahun <getahuntalaku@gmail.com>
2024-10-18 18:15:15 +0300

    Styling is implemented for SearchMovie component

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -17,24 +17,24 @@ function SearchMovie() {
     };
 
     return (
-        <div>
-            <h1> Search Movies </h1>
-            <form onSubmit={handleSearch} className=''>
+        <div className="p-4">
+            <h1 className="text-2xl mb-4"> Search Movies </h1>
+            <form onSubmit={handleSearch} className="mb-4">
                 <input type="text"
                        value={query}
                        onChange={(e) => setQuery(e.target.value)}
-                       className=""
-                       placeholder="Search for Movie" 
+                       className="boarder boarder-gray-300 p-2 rounded "
+                       placeholder="Search for Movie..." 
                        />
-                <button type="name"
-                        className="">
+                <button type="submit"
+                        className="bg-blue-500 text-white px-4 py-2 rounded ml-2">
                     Search
                 </button>
             </form>
 
-            <div className="">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
                 {movies.length > 0 ? (
-                    movies.map(() => <MovieCard key={movie.id} movie={movie} addFavorite={addFavorite} />)
+                    movies.map((movie) => <MovieCard key={movie.id} movie={movie} addFavorite={addFavorite} />)
                 ) : (
                     <p>No Results Found.</p>
                 )}
